47bb418b5b68f4a06c4db267b4fbeb020b74ce5d
Roy Mamon <mamon8521@gmail.com>
2024-12-12 18:07:05 +0200

    Added 2 functions: getComputerChoice, getHumanChoice.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
 console.log("Hey, welcome to Rock-Paper-Scissors!");
 function getComputerChoice() {
     let c = Math.floor(Math.random()*3);
     switch(c){
         case 0:
-            return "Rock";
+            return "Opponent: Rock";
         case 1:
-            return "Paper";
+            return "Opponent: Paper";
         case 2:
-            return "Scissors";            
+            return "Opponent: Scissors";            
     }
 }
 console.log(getComputerChoice());
+function getHumanChoice() {
+    let playersChoice = prompt("Choose: Rock, Paper, Scissors");
+    switch (playersChoice.toLowerCase()) {
+        case "rock":
+            return "Player: Rock";
+        case "paper":    
+            return "Player: Paper"  
+        case "scissors":    
+            return "Player: Scissors" 
+        default:
+            alert("Invalid Input, Please Try Again");
+            return getHumanChoice();     
+    }
+}
+console.log(getHumanChoice());
+function playRound(humanChoise,computerChoise){
+    
+}
